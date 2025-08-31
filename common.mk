99fca03f348cabce69daf85ba097c15872cd343f
chandu078 <chandudyavanapelli03@gmail.com>
2025-08-31 06:13:29 +0000

    sm8650-common: Switch to QTI system common components
    Change-Id: I15aeeb6610732af4a58c7349d5ca4d0305a98821
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -289,7 +289,6 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
-$(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aosp
@@ -335,6 +334,18 @@ $(call soong_config_set,qtipower,mode_ext_lib,power-ext-oplus)
 PRODUCT_PACKAGES += \
     qspa_vendor.rc \
     vendor.qti.qspa-service
+
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    alarm \
+    audio \
+    av \
+    bt \
+    display \
+    gps \
+    overlay \
+    perf \
+    wfd
 
 # SecureElement
 ifneq ($(TARGET_IS_TABLET),true)
