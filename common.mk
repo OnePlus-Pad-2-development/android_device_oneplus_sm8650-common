710997a0c300b015bc9f16e8e783374aa8f37d90
inferno0230 <mail@inferno0230.in>
2026-01-28 15:32:40 +0530

    sm8650-common: Create RRO for LineageSdk
    Change-Id: I1c572e0c28a252cda6538a88022c8dfb11044da7
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -304,6 +304,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     FrameworksResTarget \
+	LineageSdkCommon \
     NcmTetheringOverlay \
     OPlusFrameworksResCommon \
     OPlusSettingsResCommon \
